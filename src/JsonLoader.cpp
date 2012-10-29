9e906473e127f7aa6c07c072856359257834436f
Daniel Wiberg <wibbe@blueeye.se>
2012-10-29 21:26:38 +0100

    Started to implement a JSON scene loader

---
+++ src/JsonLoader.cpp
@@ -0,0 +1,20 @@
+
+#include "JsonLoader.h"
+
+#include "Object.h"
+#include "Mesh.h"
+#include "PlaneGeometry.h"
+#include "CubeGeometry.h"
+#include "DefaultMaterials.h"
+
+namespace three {
+
+  Object * JsonLoader::loadScene(const char * filename)
+  {
+    Object * root = 0;
+
+
+    return root;
+  }
+
+}
