d929398839e8f306a2899122ee64b6abe6faf62e
Daniel Wiberg <wibbe@blueeye.se>
2012-10-30 20:54:06 +0100

    More work on the json scene parser

--- src/JsonLoader.cpp
+++ src/JsonLoader.cpp
@@ -7,14 +7,86 @@
 #include "CubeGeometry.h"
 #include "DefaultMaterials.h"
 
+#include <parson.h>
+
 namespace three {
+
+  static void parseChildArray(JSON_Array * array, Object * root);
+
+  static Object * parseMesh(JSON_Object * value)
+  {
+    return 0;
+  }
+
+  static Object * parseAmbientLight(JSON_Object * value)
+  {
+    return 0;
+  }
+
+  static Object * parseObject(JSON_Object * jsonObject)
+  {
+    const char * type = json_object_get_string(jsonObject, "type");
+    const char * name = json_object_get_string(jsonObject, "name");
+
+    if (!type)
+      return 0;
+
+    Object * object = 0;
+
+    if (strcmp(type, "Mesh") == 0)
+      object = parseMesh(jsonObject);
+    else if (strcmp(type, "AmbientLight") == 0)
+      object = parseAmbientLight(jsonObject);
+    else if (strcmp(type, "Object") == 0)
+      object = new Object();
+    else
+    {
+      fprintf(stderr, "Unknown object type '%s'\n", type);
+      return 0;
+    }
+
+    if (object)
+    {
+      object->name = name ? std::string(name) : std::string("NoName");
+
+      // Parse child objects
+      if (JSON_Value * children = json_object_get_value(jsonObject, "children"))
+        if (json_value_get_type(children) == JSONArray)
+          parseChildArray(json_value_get_array(children), object);
+    }
+
+
+    return object;
+  }
+
+  static void parseChildArray(JSON_Array * array, Object * root)
+  {
+    for (size_t i = 0, len = json_array_get_count(array); i < len; ++i)
+    {
+      if (JSON_Object * member = json_array_get_object(array, i))
+        if (Object * object = parseObject(member))
+          root->add(object);
+    }
+  }
 
   Object * JSONLoader::loadScene(const char * filename)
   {
-    Object * root = 0;
+    JSON_Value * root = json_parse_file(filename);
+    if (!root)
+      return 0;
 
+    if (json_value_get_type(root) != JSONArray)
+    {
+      json_value_free(root);
+      return 0;
+    }
 
-    return root;
+    Object * rootObject = new Object();
+
+    parseChildArray(json_value_get_array(root), rootObject);
+
+    json_value_free(root);
+    return rootObject;
   }
 
 }
