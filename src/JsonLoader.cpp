cf77da9f4ad84cdd580d7d97bb66f78ebdca7e8c
Daniel Wiberg <wibbe@blueeye.se>
2012-10-30 21:14:09 +0100

    Added Vector3 parsing

--- src/JsonLoader.cpp
+++ src/JsonLoader.cpp
@@ -13,6 +13,18 @@ namespace three {
 
   static void parseChildArray(JSON_Array * array, Object * root);
 
+  static Vector3 parseVector3(JSON_Value * value)
+  {
+    if (json_value_get_type(value) != JSONArray)
+      return Vector3();
+
+    JSON_Array * array = json_value_get_array(value);
+    if (json_array_get_count(array) < 3)
+      return Vector3();
+
+    return Vector3(json_array_get_number(array, 0), json_array_get_number(array, 1), json_array_get_number(array, 2));
+  }
+
   static Object * parseMesh(JSON_Object * value)
   {
     return 0;
@@ -48,6 +60,7 @@ namespace three {
     if (object)
     {
       object->name = name ? std::string(name) : std::string("NoName");
+      object->position = parseVector3(json_object_get_value(jsonObject, "position"));
 
       // Parse child objects
       if (JSON_Value * children = json_object_get_value(jsonObject, "children"))
